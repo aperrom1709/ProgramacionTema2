fc4543db62adbacf3f4484bb9094df56fb54ccab
aperrom1709 <151721555+aperrom1709@users.noreply.github.com>
2024-11-13 10:51:27 +0100

    Add files via upload
    primera adicion de las actividades

---
+++ Ejercicio30.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package programacion.ut2;
+
+/**
+ *
+ * @author 1DAW2425-03
+ */
+public class Ejercicio30 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        for (int i = 0; i < 8; i++) {
+            
+            for (int j = 0; j < 8; j++) {
+                System.out.println(i+" "+j);
+                
+            }
+        }
+    }
+}
